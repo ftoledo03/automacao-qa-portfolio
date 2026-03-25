982042688231b8d00604f0c43abd1ccb81520c61
Fernando <jftoledoqa@gmail.com>
2026-03-25 12:27:02 -0300

    fix: correct cart items selector

--- selectors/CartSelectors.ts
+++ selectors/CartSelectors.ts
@@ -1,5 +1,5 @@
 export const CartSelectors = {
-  LIST_CART_ITEMS:      '[data-test="cart-list"]',
+  LIST_CART_ITEMS:      '[data-test="inventory-item"]',
   BUTTON_CHECKOUT:      '[data-test="checkout"]',
   BUTTON_CONTINUE_SHOP: '[data-test="continue-shopping"]',
   BUTTON_REMOVE:        '[data-test="remove-sauce-labs-backpack"]',
